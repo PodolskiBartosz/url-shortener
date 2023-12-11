aff85bf9a09a5b2f32c91f806883b670d7cea7fc
Bartosz Podolski <108195711+BartekPodolski@users.noreply.github.com>
2023-12-11 21:58:37 +0100

    use session instead of local storage for authentication

--- src/app/core/services/session-manager/session-manager.service.ts
+++ src/app/core/services/session-manager/session-manager.service.ts
@@ -15,18 +15,18 @@ const USER_ID_STORAGE_KEY = 'user_id';
   providedIn: 'root',
 })
 export class SessionManager {
-  // Stores an access session token along with expiration and user id in local storage.
+  // Stores an access session token along with expiration and user id in session storage.
   public createAccessSession(info: LoginInfo) {
     this.setNewAccessSessionExpiration();
-    localStorage.setItem(SESSION_TOKEN_STORAGE_KEY, info.token);
-    localStorage.setItem(USER_ID_STORAGE_KEY, getIdByUsername(info.username));
+    sessionStorage.setItem(SESSION_TOKEN_STORAGE_KEY, info.token);
+    sessionStorage.setItem(USER_ID_STORAGE_KEY, getIdByUsername(info.username));
   }
 
-  // Returns a session token from local storage.
+  // Returns a session token from session storage.
   // If the session token couldn't be found or is expired, then an empty string is returned instead.
   public getCurrentAccessSession() {
-    const expirationString = localStorage.getItem(EXPIRATION_STORAGE_KEY);
-    const token = localStorage.getItem(SESSION_TOKEN_STORAGE_KEY);
+    const expirationString = sessionStorage.getItem(EXPIRATION_STORAGE_KEY);
+    const token = sessionStorage.getItem(SESSION_TOKEN_STORAGE_KEY);
 
     const response: AccessSession = {
       token: token ? token : '',
@@ -41,33 +41,33 @@ export class SessionManager {
     return response;
   }
 
-  // Removes a session token from local storage along with expiration and user id.
+  // Removes a session token from session storage along with expiration and user id.
   public deleteCurrentAccessSession() {
-    localStorage.removeItem(EXPIRATION_STORAGE_KEY);
-    localStorage.removeItem(SESSION_TOKEN_STORAGE_KEY);
-    localStorage.removeItem(USER_ID_STORAGE_KEY);
+    sessionStorage.removeItem(EXPIRATION_STORAGE_KEY);
+    sessionStorage.removeItem(SESSION_TOKEN_STORAGE_KEY);
+    sessionStorage.removeItem(USER_ID_STORAGE_KEY);
   }
 
   // Sets new expiration of 30 minutes for the current session.
   public setNewAccessSessionExpiration() {
     const today = new Date();
     const expiration = date.addMinutes(today, 30, true);
-    localStorage.setItem(EXPIRATION_STORAGE_KEY, expiration.toUTCString());
+    sessionStorage.setItem(EXPIRATION_STORAGE_KEY, expiration.toUTCString());
   }
 
-  // Returns username based on stored id in local storage.
+  // Returns username based on stored id in session storage.
   // If the user id couldn't be found, then an empty string is returned instead.
   public getUsernameFromSession() {
-    const id = localStorage.getItem(USER_ID_STORAGE_KEY);
+    const id = sessionStorage.getItem(USER_ID_STORAGE_KEY);
     if (id) {
       return getUsernameById(id);
     }
     return '';
   }
 
-  // Returns user id from local storage.
+  // Returns user id from session storage.
   public getUserIdFromSession() {
-    const userId = localStorage.getItem(USER_ID_STORAGE_KEY);
+    const userId = sessionStorage.getItem(USER_ID_STORAGE_KEY);
     return userId ? userId : '';
   }
 }
