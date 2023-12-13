eaca1c243457777365ea0cb9ae52043a70f4f53a
Bartosz Podolski <108195711+BartekPodolski@users.noreply.github.com>
2023-12-13 22:04:03 +0100

    add comments

--- src/app/core/services/url/url.service.ts
+++ src/app/core/services/url/url.service.ts
@@ -9,18 +9,21 @@ import { URLS_ENDPOINT } from '../../../shared/constants/endpoints';
 export class UrlService {
   constructor(private _httpClient: HttpClient) {}
 
+  // Creates a new url for the current user
   public createUrl(urlItem: UrlItem) {
     return this._httpClient.post<void>(URLS_ENDPOINT, urlItem, {
       observe: 'response',
     });
   }
 
+  // Gets all urls for the current user
   public getAllUrls() {
     return this._httpClient.get<Array<UrlItem>>(URLS_ENDPOINT, {
       observe: 'response',
     });
   }
 
+  // Deletes a specified url of the current user
   public deleteUrl(id: string) {
     return this._httpClient.delete<void>(`${URLS_ENDPOINT}/${id}`, {
       observe: 'response',
